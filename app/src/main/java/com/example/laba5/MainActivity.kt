fb02645095ddc6fd906b0e79ee4ba5a2952fde30
Анастасия <azizovaa031@gmail.com>
2024-11-02 04:07:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/laba5/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.laba5
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    public lateinit var checkbox1: CheckBox
+    public lateinit var checkbox2: CheckBox
+    public lateinit var checkbox3: CheckBox
+    public lateinit var count: EditText
+    public lateinit var button: Button
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
